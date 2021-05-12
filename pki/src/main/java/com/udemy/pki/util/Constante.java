301f319610b96acf666a8969f6aeafc2d209abd2
John Vo <vojohn95@gmail.com>
2021-05-12 18:19:33 -0500

    El proyecto lista la información de firma y el almacen

---
+++ pki/src/main/java/com/udemy/pki/util/Constante.java
@@ -0,0 +1,6 @@
+package com.udemy.pki.util;
+
+public class Constante {
+	public static final String CERTIFICADO = "C:\\Users\\52552\\Documents\\PKI-java\\john.pfx´";
+	public static final String CLAVE = "123456789";
+}
